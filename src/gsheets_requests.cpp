d5d1ee2102afedb7c5ce1abbec71c6b7578f1004
Archie Wood <58074498+archiewood@users.noreply.github.com>
2024-10-24 02:14:57 -0400

    fix reads: use HTTP/1.0

--- src/gsheets_requests.cpp
+++ src/gsheets_requests.cpp
@@ -45,7 +45,7 @@ namespace duckdb
             break;
         }
 
-        std::string request = method_str + " " + path + " HTTP/1.1\r\n";
+        std::string request = method_str + " " + path + " HTTP/1.0\r\n";
         request += "Host: " + host + "\r\n";
         request += "Authorization: Bearer " + token + "\r\n";
         request += "Connection: close\r\n";
@@ -81,7 +81,6 @@ namespace duckdb
         SSL_CTX_free(ctx);
 
         // Extract body from response
-        // Extract body from response
         size_t body_start = response.find("\r\n\r\n");
         if (body_start != std::string::npos)
         {
@@ -100,7 +99,6 @@ namespace duckdb
             path += "?valueInputOption=RAW";
         }
 
-        return perform_https_request(host, path, token, method, body);
         return perform_https_request(host, path, token, method, body);
     }
 }
